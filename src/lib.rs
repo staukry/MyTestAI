51841145ac8d4225cda5a44290d85e5b2f0f1b11
staukry <shoong1105@gmail.com>
2025-08-19 19:22:36 +0900

    can read csv file!!!

--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,17 @@ pub mod A {
         use std::fs::File;
         use std::io::Read;
 
-        pub fn read_csv(Input:String) -> std::io::Result<()> {
+        pub fn read_csv(Input:String) -> std::io::Result<Vec<String>> {
             let mut F = File::open(Input)?;
             let mut data = String::new();
             F.read_to_string(&mut data)?;
-
             let _ = data.retain(|c| c != '\r');
-            // let Targets: &[_] = &['\r'];
-            // let mut buffers = data.trim_matches(Targets);
-            println!("{:?}",data);
-            Ok(())
+            let mut Vec :Vec<String> = Vec::new();
+            let lines= data.lines();
+            for Line in lines {
+                Vec = Line.split(',').map(|T| T.to_string()).collect();
+            }
+            Ok(Vec)
         }
     }
     // pub mod basic {
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ fn main() {
         let mut buffer: String = String::new();
         let _:String= stdin().read_line(&mut buffer).expect("a").to_string();
         let input = buffer.trim().to_string();
-        let _ = read_csv(input);
+        let Results: Result<Vec<String>, io::Error> = read_csv(input);
+        println!("{:?}", Results);
     }
     
 }
