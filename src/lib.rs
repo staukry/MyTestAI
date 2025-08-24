b5b20b7183e8e74c9b4b8f79d2d7360e88f18dbe
staukry <shoong1105@gmail.com>
2025-08-24 17:24:27 +0900

    really can read csv File!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 
 
 pub mod Cacls {
-    pub mod AAA{
+    pub mod cacls{
         pub fn Euclidean_distance(Target1: (f64, f64),Target2: (f64, f64)) -> f64 {
             let F = Target1.0 - Target2.0;
             let S = Target1.1 - Target2.1;
@@ -15,18 +15,21 @@ pub mod A {
     pub mod data{
         use std::fs::File;
         use std::io::Read;
-        pub fn read_csv(Input:String) -> std::io::Result<Vec<String>> {
+        use std::collections::HashMap;
+        pub fn read_csv(Input:String) -> std::io::Result<HashMap<u32, Vec<String>>> {
             let mut F = File::open(Input)?;
             let mut data = String::new();
             F.read_to_string(&mut data)?;
             let _ = data.retain(|c| c != '\r');
-            let mut Vec :Vec<String> = Vec::new();
+            let mut table: HashMap<u32, Vec<String>> = HashMap::new();
             let lines= data.lines();
+            let mut line_number:u32 = 0;
             for Line in lines {
-                Vec = Line.split(',').map(|T| T.to_string()).collect();//vec!( Vec![],Vec![]......) <------- ????
-                println!("{:?}", Vec);
+                table.insert(line_number, Line.split(',').map(|T| T.to_string()).collect());//vec!( Vec![],Vec![]......) <------- ????
+                // println!("{:?}", table);
+                line_number += 1;
             }
-            Ok(Vec)
+            Ok(table)
         }
     }
     pub mod K_Series {
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 #![allow(non_snake_case)]
 use mai::A::data::*;
-use std::io::{self, stdin};
+use std::{collections::HashMap, io::{self, stdin}};
 
 fn main() {
+    // let mut table: HashMap<u32, Vec<f64>> = HashMap::new(); <---- this code is table code
     loop {
         let mut buffer: String = String::new();
         let _:String= stdin().read_line(&mut buffer).expect("a").to_string();
         let input = buffer.trim().to_string();
-        let Results: Result<Vec<String>, io::Error> = read_csv(input);
-        println!("{:?}", Results);
+        let Results: Result<HashMap<u32, Vec<String>>, io::Error> = read_csv(input);
+        println!("{:#?}", Results);
     }
 }
